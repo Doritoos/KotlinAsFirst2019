54b5efd448fc5814d0a050d002012838cf62b5e1
Doritoos <zda.post@gmail.com>
2019-10-31 11:29:22 +0300

    ...

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -3,6 +3,7 @@
 package lesson4.task1
 
 import lesson1.task1.discriminant
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 /**
@@ -115,14 +116,26 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double =
+    if (v.isEmpty()) 0.0
+    else {
+        var s = 0.0
+        for (i in 0 until v.size)
+            s += v[i] * v[i]
+        sqrt(s)
+    }
+
 
 /**
  * Простая
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+
+fun mean(list: List<Double>): Double =
+    if (list.isEmpty()) 0.0
+    else list.sum() / list.size
+
 
 /**
  * Средняя
@@ -132,7 +145,12 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    val d = mean(list)
+    for (i in 0 until list.size)
+        list[i] -= d
+    return list
+}
 
 /**
  * Средняя
@@ -141,7 +159,15 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = TODO()
+fun times(a: List<Int>, b: List<Int>): Int {
+    var c = 0
+    return if (a.isEmpty() || b.isEmpty()) c
+    else {
+        for (i in 0 until a.size)
+            c += a[i] * b[i]
+        c
+    }
+}
 
 /**
  * Средняя
@@ -151,7 +177,16 @@ fun times(a: List<Int>, b: List<Int>): Int = TODO()
  * Коэффициенты многочлена заданы списком p: (p0, p1, p2, p3, ..., pN).
  * Значение пустого многочлена равно 0 при любом x.
  */
-fun polynom(p: List<Int>, x: Int): Int = TODO()
+fun polynom(p: List<Int>, x: Int): Int {
+    var g = 0.0
+   return if (p.isEmpty()) 0
+    else {
+        for (i in 0 until p.size)
+            g += p[i] * x.toDouble().pow(i)
+    g.toInt()
+   }
+
+}
 
 /**
  * Средняя
